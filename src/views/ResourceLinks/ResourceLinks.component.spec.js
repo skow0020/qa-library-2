6aadcab898cf1f9de705ebeb72883779171ed3dd
Colin Skow <colin.skow@solutiondesign.com>
2022-05-19 15:57:58 -0500

    Update ResourceLinks

---
+++ src/views/ResourceLinks/ResourceLinks.component.spec.js
@@ -0,0 +1,44 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import { clickDropdown } from 'testHelpers/rtlHelpers';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import { createServer } from "miragejs"
+
+import ResourceLinks from './ResourceLinks';
+import { resourceLinks } from './testData';
+
+let server
+
+beforeEach(() => {
+  server = createServer({
+    environment: "test",
+    urlPrefix: "https://qa-library-dev.herokuapp.com/api",
+  })
+})
+
+afterEach(() => {
+  server.shutdown()
+})
+
+describe('ResourceLinks Unit Tests', () => {
+  test('ResourceLinks renders', async () => {
+    const user = userEvent.setup()
+    server.get("/resourceLinks", () => resourceLinks)
+
+    render(
+      <Router>
+        <ResourceLinks />
+      </Router>);
+      
+    await screen.findByText('ResourceLinks')
+    screen.getByRole('button', { name: 'Add Resource Link' })
+    screen.getAllByText('How to sand a hippo')
+    await clickDropdown(user, 'Language', 'Swift')
+    await screen.findAllByText('How to sand a hippo')
+    await clickDropdown(user, 'Category', 'General')
+    
+    await screen.findAllByText('How to sand a hippo')
+    expect(screen.queryByText('Python')).toBeNull()
+    expect(screen.queryByText('Swift')).not.toBeNull()
+  });
+});
